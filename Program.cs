620f3ee7b9ae44bf0d6b16e8e84d47e93a974952
MIT4893-Projects <minhngoc2891211@gmail.com>
2023-03-11 07:23:56 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace TimeExecute
+{
+    /// <summary>
+    /// Program's controller class.
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// This function will invoke when the program start.
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            //
+        }
+    }
+}
