c799f157e27b78e49946e669b7de5d321e97a162
MIT4893-Projects <minhngoc2891211@gmail.com>
2023-03-12 14:58:54 +0700

    Add ParametersParser class

---
+++ Components/Parsers/ParametersParser.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.RegularExpressions;
+
+namespace TimeExecute.Components.Parsers
+{
+    /// <summary>
+    /// Parse and check parameter's string.
+    /// </summary>
+    static class ParametersParser
+    {
+        /// <summary>
+        /// Check a parameter is option parameter or not.
+        /// </summary>
+        /// <param name="parameter">Parameter to check.</param>
+        /// <returns>The passed parameter is option parameter or not.</returns>
+        public static bool IsOptionParameter(string parameter)
+        {
+            // An option parameter starts with '-' character.
+            string pattern = "^-||^--";
+            return Regex.Match(parameter, pattern).Success;
+        }
+    }
+}
