059fb5b81d6c65812ebd0d86b0395617f4578d73
MIT4893-Projects <minhngoc2891211@gmail.com>
2023-03-12 14:56:59 +0700

    Add NoCommandParameterToExecuteException

---
+++ Components/Exceptions/ParametersExceptions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeExecute.Components.Exceptions
+{
+    class NoCommandParameterToExecuteException : Exception
+    {
+        /// <summary>
+        /// This exception occurs when the program start without any command or application to timing.
+        /// </summary>
+        /// <param name="message"></param>
+        public NoCommandParameterToExecuteException(
+            string message = "There aren't any file paths or commands for timing.") : base(message) { }
+    }
+}
