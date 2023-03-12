33b053eb07848b1b8e04bedde9b491008e12e186
MIT4893-Projects <minhngoc2891211@gmail.com>
2023-03-12 15:05:32 +0700

    Add ApplicationParametersHandler class

---
+++ Components/Handlers/ApplicationParametersHandler.cs
@@ -0,0 +1,23 @@
+﻿using TimeExecute.Components.Exceptions;
+
+namespace TimeExecute.Components.Handlers
+{
+    class ApplicationParametersHandler
+    {
+        private readonly List<Parameter> Parameters = new();
+
+        public ApplicationParametersHandler(string[] parameters)
+        {
+            if (parameters.Length == 0)
+                throw new NoCommandParameterToExecuteException();
+
+            ParseParameters(parameters);
+        }
+
+        private void ParseParameters(string[] parameters)
+        {
+            foreach (string parameter in parameters)
+                Parameters.Add(new(parameter));
+        }
+    }
+}
