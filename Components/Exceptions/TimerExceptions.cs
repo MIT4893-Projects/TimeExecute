f2cb2ff9cd7e3e758e727bcd5e187d97b9761d83
MIT4893-Projects <minhngoc2891211@gmail.com>
2023-03-12 14:58:17 +0700

    Add TimerExceptions
     - Add UnstartedTimerException class
     - Add StartedTimerException class

---
+++ Components/Exceptions/TimerExceptions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeExecute.Components.Exceptions
+{
+    class UnstartedTimerException : Exception
+    {
+        /// <summary>
+        /// This Exception occurs when stop Timer without started it.
+        /// </summary>
+        public UnstartedTimerException(
+            string message = "The timer haven't start yet.") : base(message) { }
+    }
+
+    class StartedTimerException : Exception
+    {
+        /// <summary>
+        /// This Exception occurs when start Timer whether it have been started.
+        /// </summary>
+        public StartedTimerException(
+            string message = "The timer already started.") : base(message) { }
+    }
+}
