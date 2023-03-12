586d69c56a12c785368f1d49fffab87199a3b8af
MIT4893-Projects <minhngoc2891211@gmail.com>
2023-03-12 14:59:23 +0700

    Add Parameter class

---
+++ Components/Parameter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TimeExecute.Components.Parsers;
+
+namespace TimeExecute.Components
+{
+    /// <summary>
+    /// Represents an application parameter.
+    /// </summary>
+    class Parameter
+    {
+        /// <summary>
+        /// Get a value whether this Parameter is option parameter.
+        /// </summary>
+        public readonly bool IsOption = false;
+
+        /// <summary>
+        /// Get name of this Parameter.
+        /// </summary>
+        public readonly string Name;
+
+        /// <summary>
+        /// Create an application parameter from name.
+        /// </summary>
+        /// <param name="name">Name of the parameter.</param>
+        public Parameter(string name)
+        {
+            IsOption = ParametersParser.IsOptionParameter(name);
+            Name = name;
+        }
+    }
+}
