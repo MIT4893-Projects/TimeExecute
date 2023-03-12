1ece00b6749d2087ef2f89c27ddeb0629b8ebaf4
MIT4893-Projects <minhngoc2891211@gmail.com>
2023-03-12 15:02:01 +0700

    Add TimeSpanConverter class

---
+++ Components/TimeSpanConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeExecute.Components
+{
+    static class TimeSpanConverter
+    {
+        public static string ToMinuteSecond(TimeSpan time)
+        {
+            return time.ToString(@"m\:ss\.fff");
+        }
+
+        public static string ToSecond(TimeSpan time)
+        {
+            return time.ToString(@"s\.fff");
+        }
+    }
+}
